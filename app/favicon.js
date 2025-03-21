5f0508308737e9ff0463de65456eb2ccf9becdaf
StefanRu <StefanRu@users.noreply.github.com>
2025-03-21 16:44:02 +0100

    Favico issue

---
+++ app/favicon.js
@@ -0,0 +1,10 @@
+// This file is used to generate a favicon
+// Next.js will automatically use this file to generate a favicon
+
+export default function Favicon() {
+  return {
+    rel: "icon",
+    url: "/favicon.ico",
+  }
+}
+
